48074b8eab28e2aa21810d51b5599aa217d478b6
Peter Crona <petercrona89@gmail.com>
2021-02-19 18:42:19 +0100

    handle when no or just 1 value

--- StreamStats.cpp
+++ StreamStats.cpp
@@ -8,9 +8,13 @@ StreamStats::StreamStats() {
 }
 
 void StreamStats::reportNumber(int n) {
-  double prevMean = currentMean_;
-  currentMean_ += ((n - prevMean) / (nrValues_ + 1));
-  currentVarianceHelperValue_ += (n - prevMean) * (n - currentMean_);
+  if (nrValues_ == 0) {
+    currentMean_ = n;
+  } else {
+    double prevMean = currentMean_;
+    currentMean_ += ((n - prevMean) / (nrValues_ + 1));
+    currentVarianceHelperValue_ += (n - prevMean) * (n - currentMean_);
+  }
   nrValues_++;
 }
 
@@ -19,7 +23,11 @@ double StreamStats::average() {
 }
 
 double StreamStats::variance() {
-  return currentVarianceHelperValue_ / (nrValues_ - 1);
+  if (nrValues_ > 1) {
+    return currentVarianceHelperValue_ / (nrValues_ - 1);
+  } else {
+    return 0;
+  }
 }
 
 double StreamStats::stdDev() {
